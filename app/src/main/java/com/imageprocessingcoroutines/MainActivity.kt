d0b76d5a787ac3f77d481e8ca9906f11c19f3ec2
Pooja Mantri <pooja.mantri@ibm.com>
2022-11-01 17:15:44 +0530

    First Commit

---
+++ app/src/main/java/com/imageprocessingcoroutines/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.imageprocessingcoroutines
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.DiscretePathEffect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO){ getOriginalImage() }
+            val originalImage = originalDeferred.await()
+
+            val filteredDeferred = coroutineScope.async(Dispatchers.Default) { applyFilter(originalImage) }
+            val filterImage = filteredDeferred.await()
+            loadImage(filterImage)
+        }
+    }
+
+    fun getOriginalImage()=
+        URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    fun loadImage(bmp:Bitmap){
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
+    }
+
+    fun applyFilter(bmp: Bitmap)= Filter.apply(bmp)
+
+
+}
